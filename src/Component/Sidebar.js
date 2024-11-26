9a056ebea2ececcf6e5bc851561f4a10a6376959
parkduhwan <enghksqkr05@gmail.com>
2024-11-26 16:22:20 +0900

    fix: useState 빌드 오류

--- src/Component/Sidebar.js
+++ src/Component/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useNavigate, NavLink } from "react-router-dom";
 import styles from "../assets/style/Sidebar.module.scss";
 
