9956578e5882caac48730bc51bbcbe1b650a84ff
simjunseo <01041775428a@gmail.com>
2025-01-24 18:32:39 +0900

    feat: 사진첩 페이지 생성성

---
+++ src/page/Picture.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../src/assets/style/Picture/Picture.module.scss";
+
+const Picture = () =>{
+    return(
+        <div className={styles.PictureConstainer}>
+
+        </div>
+    );
+};
+
+export default Picture;
