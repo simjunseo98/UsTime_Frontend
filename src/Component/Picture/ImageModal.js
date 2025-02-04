6cdbbb7dd4814036d207e3c2c91f77a28ac15d63
simjunseo <01041775428a@gmail.com>
2025-02-04 20:27:22 +0900

    test

--- src/Component/Picture/ImageModal.js
+++ src/Component/Picture/ImageModal.js
@@ -1,10 +1,10 @@
-import React from "react";
+// import React from "react";
 
-const ImageModal = ()=>{
-    return(
-        <>
-        </>
-    );
-};
+// const ImageModal = ()=>{
+//     return(
+//         <>
+//         </>
+//     );
+// };
 
-export default ImageModal;
+// export default ImageModal;
