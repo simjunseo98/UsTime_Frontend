d5c380e54c12af6d61a026927698d725e5f1bce9
simjunseo <01041775428a@gmail.com>
2025-02-04 21:11:09 +0900

    test

--- src/Component/Picture/ImageModal.js
+++ src/Component/Picture/ImageModal.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-const ImageModal = ()=>{
-    return(
+const ImageModal = () => {
+    return (
         <>
+            <h2>이미지 모달</h2>
+            {/* 여기에 모달의 실제 내용을 추가할 수 있습니다 */}
         </>
     );
 };
