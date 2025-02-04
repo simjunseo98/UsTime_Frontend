d364f7f98ed417ec7b1004a89ac0d94bcebd3d78
simjunseo <01041775428a@gmail.com>
2025-02-04 15:00:27 +0900

    fix: 빌드 실패 수정정

--- src/Component/Picture/Album.js
+++ src/Component/Picture/Album.js
@@ -1,29 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../../assets/style/Picture/Picture.module.scss";
+import api from "../../service/api.js";
+import Loading from "../../Component/Common/Loading.js";
+
 
 const Album =() =>{
-// const [picture, setPicture]= useState([]);
-// const [loading, setLoading] = useState(true);
-// const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [picture, setPicture]= useState([]);
 
-// useEffect(() =>{
-//     const getPictures = async () => {
-//         try{
-//             const response= await api.get("");
-//             setPicture(response.data);
-//             setLoading(false);
-//         }catch(error){
-//           setError();
-//           setLoading(false);
-//         }
-//     }
-//  getPictures();
-// },[]);
+useEffect(() =>{
+    const getPictures = async () => {
+        try{
+            const response= await api.get("");
+            setPicture(response.data);
+            setLoading(false);
+        }catch(error){
+          setError();
+          setLoading(false);
+        }
+    }
+ getPictures();
+},[]);
 
+if (loading) return <Loading/>;
+if (error) return <p>Error: {error}</p>;
 
     return(
         <div className={styles.PictureAlbum}>
-            <img alt="사진들" className={styles.image}></img>
+              {picture.map((image, index) => (
+                image.url ? (
+                    <img key={index} src={image.url} alt="Gallery"/>
+                ) : (
+                    <p key={index}>이미지를 불러올 수 없습니다.</p>
+                )
+            ))}
         </div>
     );
 };
