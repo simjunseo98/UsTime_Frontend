bb23ee7b37e740252fe0a59f06d3214fab0a1c51
simjunseo <01041775428a@gmail.com>
2025-02-10 19:46:09 +0900

    feat: 사진첩 모달 및 서버연결

--- src/Component/Picture/ImageModal.js
+++ src/Component/Picture/ImageModal.js
@@ -1,12 +1,17 @@
-// import React from "react";
+import React from "react";
+import styles from '../../assets/style/Picture/ImageModal.module.scss'; 
 
-// const ImageModal = () => {
-//     return (
-//         <>
-//             <h2>이미지 모달</h2>
-//             {/* 이미지 모달 구상 */}
-//         </>
-//     );
-// };
+const ImageModal = ( {image, onClose}) => {
+    if(!image) return null;
+    return (
+        <div className={styles.ModalOverlay} onClick={onClose}>
+              <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+                <button className={styles.closeButton} onClick={onClose}>X</button>
+                <img src={image.photoUrl} alt={image.photoTitle} className={styles.modalImage} />
+                <p>{image.photoTitle}</p>
+            </div>
+        </div>
+    );
+};
 
-// export default ImageModal;
+export default ImageModal;
