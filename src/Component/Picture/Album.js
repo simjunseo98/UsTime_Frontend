7605c66549e7ed2f8924bbe65d6eb104556b9be2
parkduhwan <enghksqkr05@gmail.com>
2025-02-12 18:21:17 +0900

    fix: 빌드 오류 수정

--- src/Component/Picture/Album.js
+++ src/Component/Picture/Album.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../../assets/style/Picture/Picture.module.scss";
 import api from "../../service/api.js";
 import Loading from "../../Component/Common/Loading.js";
@@ -8,16 +8,6 @@ const Album =({coupleId,onImageClick}) =>{
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [picture, setPicture]= useState([]);
-
-     // useMemo로 tempImage 메모이제이션
-  const tempImage= useMemo(() => [
-    { photoId: 1, photoUrl: "https://picsum.photos/800/600"},
-    { photoId: 2, photoUrl: "https://picsum.photos/800/600"},
-    { photoId: 3, photoUrl: "https://picsum.photos/800/600"},
-    { photoId: 4, photoUrl: "https://picsum.photos/800/600"},
-    { photoId: 5, photoUrl: "https://picsum.photos/800/600"},
-    { photoId: 6, photoUrl: "https://picsum.photos/800/600" }
-  ], []); // 의존성 배열이 빈 배열이므로 한 번만 생
 
 useEffect(() =>{
     if(!coupleId) return;
@@ -30,8 +20,7 @@ useEffect(() =>{
             console.log("✅ API 응답 성공:", response);
             console.log("사진데이터",response.data);
             if(response.data.length===0){
-                // setPicture( Array(9).fill({ photoId: null, photoUrl: notimage, photoTitle: "기본 이미지" }));
-                setPicture(tempImage);
+                setPicture( Array(9).fill({ photoId: null, photoUrl: notimage, photoTitle: "기본 이미지" }));
             }else{
                 setPicture(response.data);
             }
@@ -44,17 +33,16 @@ useEffect(() =>{
         }
     }
  getPictures();
-},[coupleId,tempImage]);
+},[coupleId]);
 
 if (loading) return <Loading/>;
 if (error) return <p>Error: {error.message}</p>;
 
   // ✅ 기본 이미지도 같은 개수로 맞춰서 배열 생성
-//   const displayPictures =
-//   picture.length > 0
-//       ? picture
-//       : Array(9).fill({ photoId: null, photoUrl: notimage, photoTitle: "기본 이미지" });
-const displayPictures = picture.length > 0 ? picture : tempImage;
+  const displayPictures =
+  picture.length > 0
+      ? picture
+      : Array(9).fill({ photoId: null, photoUrl: notimage });
 console.log("디스플레이 이미지:", displayPictures);
 
     return(
